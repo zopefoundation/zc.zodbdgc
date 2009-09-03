9d5cbac6b150769c580cb03ebbaa8100acbc1779
Jim Fulton <jim@zope.com>
2009-09-03 19:54:10 +0000

    Updated version.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@
 #
 ##############################################################################
 
-name, version = 'zc.zodbdgc', '0'
+name, version = 'zc.zodbdgc', '0.3.0'
 
 import os
 from setuptools import setup, find_packages
